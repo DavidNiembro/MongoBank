a540b48c5ee12fc5e18a6202b0da589c32afff48
David Niembro <david@niembro.ch>
2019-01-23 12:08:20 +0100

    add total amount

--- controllers/compte.js
+++ controllers/compte.js
@@ -5,6 +5,13 @@ exports.index = function (req, res) {
     User.findOne({_id : req.session.user.id}, function(err, result) {
         if (err) throw err;
             comptes = result.comptes;
+            comptes.forEach(function(compte,key) {
+                total = 0;
+                compte.transactions.forEach(function(transaction) {
+                        total += transaction.amount;
+                  });
+                  comptes[key].total=total
+              });
             res.render('pages/dashboard',{comptes:comptes,page:'dashboard'});
     });
 };
